ae86cc09f1c3c6dde0dc350b692c89b0fa590542
Theo Rocha <theomarquesrx@gmail.com>
2023-12-20 10:10:33 -0400

    alterações para decremento do for

--- for.asm
+++ for.asm
@@ -2,20 +2,16 @@ section .data
     var db 'loop '
     size equ $ - var
 
-%macro loopFor 4
+%macro loopFor 2
     pusha                   ;Preservar os valores dos registradores antes de qq coisa
     .loopfor:
-        %2                  ; comparação do for
+        cmp ebx, -1         ; comparação do for
         je .end_loop        ; sair do loop se a comparação for falsa
-        %3                  ; corpo do for
-        %4                  ; incrementação do for
+        %2                  ; corpo do for
+        dec ebx             ; decremento do for
         jmp .loopfor        ; chamada recursiva da função
         .end_loop:          ; restaura todos os registradores de propósito geral da pilha. Isso reverte o efeito da instrução pusha para garantir que os registradores tenham seus valores originais.
     popa
-%endmacro
-
-%macro comparacao 0
-    cmp ebx, 5              ;Contador FINAL do laço for
 %endmacro
 
 %macro imprime 0
@@ -27,19 +23,12 @@ section .data
     int 0x80                ;Syscall-Interruptor
     popa                    ;Desempilha tudo, garantindo os valores iniciais
 %endmacro
-    
-
-%macro adiciona_ao_contador 0
-    inc ebx                 ;Incrementa o contador
-%endmacro
-
-
 
 global _start               ; Inicialização do programa...
 _start:
-    mov ebx, 1              ; Contador INICIAL do laço for(Número de laços será linha 31 - 41)
+    mov ebx, 3             ; Contador INICIAL do laço for(Número de laços será linha 31 - 41)
 
-    loopFor ebx, comparacao, imprime, adiciona_ao_contador
+    loopFor ebx,imprime
 
     mov eax, 1               ; Coloca o valor 1 no registrador eax. Este valor é usado como argumento para a syscall de saída 
     xor ebx, ebx             ; Limpa o registrador ebx, garantindo que ele contenha zero antes de chamar a syscall de saída
